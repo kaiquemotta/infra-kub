bb4497a11c9631fd8cdee8e1ed1c878997483981
Kaique Motta <kaique.motta@hotmail.com>
2024-12-17 13:55:23 -0300

    feat: dev

--- main.tf
+++ main.tf
@@ -2,30 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Utilizando uma VPC existente
-data "aws_vpc" "selected" {
-  id = "vpc-015416e2606ea05dc"
-}
-
-# Declarando as sub-redes existentes dentro da VPC
-variable "subnet_ids" {
-  description = "List of subnet IDs"
-  type        = list(string)
-  default     = ["subnet-0bb1c79de3EXAMPLE", "subnet-0bb1c79de3EXAMPLE2", "subnet-0bb1c79de3EXAMPLE3"]
-}
-
-resource "aws_eks_cluster" "my_cluster" {
-  name     = "my-cluster"
-  role_arn = aws_iam_role.eks_cluster_role_new.arn
-
-  vpc_config {
-    subnet_ids = var.subnet_ids
+resource "null_resource" "check_iam_role_eks_cluster" {
+  provisioner "local-exec" {
+    command = "aws iam get-role --role-name eks_cluster_role_new || echo 'Role does not exist'"
   }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSVPCResourceController
-  ]
 }
 
 resource "aws_iam_role" "eks_cluster_role_new" {
@@ -43,6 +23,8 @@ resource "aws_iam_role" "eks_cluster_role_new" {
       }
     ]
   })
+
+  depends_on = [null_resource.check_iam_role_eks_cluster]
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
@@ -53,6 +35,67 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
 resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.eks_cluster_role_new.name
+}
+
+resource "null_resource" "check_iam_role_eks_node" {
+  provisioner "local-exec" {
+    command = "aws iam get-role --role-name eks_node_role_new || echo 'Role does not exist'"
+  }
+}
+
+resource "aws_iam_role" "eks_node_role_new" {
+  name = "eks_node_role_new"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  depends_on = [null_resource.check_iam_role_eks_node]
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role_new.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role_new.name
+}
+
+# Utilizando uma VPC existente
+data "aws_vpc" "selected" {
+  id = "vpc-015416e2606ea05dc"
+}
+
+# Declarando as sub-redes existentes dentro da VPC
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+  default     = ["subnet-0d03fd1174a3d8e42", "subnet-0ecf94d2c4e4a095a", "subnet-0e5b69a0f2630bad1", "subnet-0dc6713ec55c68387"]
+}
+
+resource "aws_eks_cluster" "my_cluster" {
+  name     = "my-cluster"
+  role_arn = aws_iam_role.eks_cluster_role_new.arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSVPCResourceController
+  ]
 }
 
 resource "aws_eks_node_group" "my_node_group" {
@@ -71,31 +114,4 @@ resource "aws_eks_node_group" "my_node_group" {
     aws_iam_role_policy_attachment.eks_node_AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.eks_node_AmazonEC2ContainerRegistryReadOnly
   ]
-}
-
-resource "aws_iam_role" "eks_node_role_new" {
-  name = "eks_node_role_new"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_node_role_new.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_node_AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_node_role_new.name
 }
